97453dd2510d72379e474d47611328ad057f07a7
Nicholas Crossman <nicholascrossman@gmail.com>
2021-11-09 17:21:23 -0800

    Added basic component skeleton to Customer.

--- dollars-bank-react-frontend/src/components/Customer.js
+++ dollars-bank-react-frontend/src/components/Customer.js
@@ -3,7 +3,26 @@ import React from "react";
 function Customer(props) {
     return (
         <div>
-            Hello {props.name}!
+            <div>
+                <h1>Your Account</h1>
+                <br/>
+
+                <h3>Name: {props.name}</h3><br/>
+                <h3>Username: {props.username}</h3><br/>
+                <h3>Address: {props.address}</h3><br/>
+                <h3>Phone: {props.phoneNumber}</h3><br/>
+                <h3>Balance: {props.currentBalance}</h3><br/>
+            </div>
+            <div>
+                <hr/>
+                <h3>Navbar Goes Here!</h3>
+                <hr/>
+            </div>
+            <div>
+                <h2>Recent Transactions</h2>
+                Put transactions here
+            </div>
+
         </div>
     );
 }
