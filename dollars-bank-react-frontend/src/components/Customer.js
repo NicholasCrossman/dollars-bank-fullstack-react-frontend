a7dc4cab3abe6b45842099b384c81e5990822c83
Nicholas Crossman <nicholascrossman@gmail.com>
2021-11-18 11:49:01 -0800

    Customer page now displays Transactions and customer info.

--- dollars-bank-react-frontend/src/components/Customer.js
+++ dollars-bank-react-frontend/src/components/Customer.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Router } from 'react-router-dom';
+import CustomerService from "../service/CustomerService";
 import NavbarBank from "./NavbarBank";
 
-function Customer(props) {
+function Customer() {
+
+    const [transactions, setTransactions] = useState([]);
+    const [account, setAccount] = useState({});
+
+    let service = new CustomerService();
+
+    useEffect(() => {
+        getCustomerInfo();
+        getAllTransactions();
+    }, []);
+
+    const getCustomerInfo = async () => {
+        let response = await service.getCustomerInfo();
+        //console.log("LoginResponse: " + JSON.stringify(response));
+        if (response.status === 200) {
+          console.log("GetCustomerInfo: Login success!");
+          //console.log(JSON.stringify(response.data));
+    
+          setAccount(response.data);
+        } else {
+          // otherwise login failed with an error message
+          console.log("Error: GetAllOthercustomers failed.");
+        }
+    }
+
+    const getAllTransactions = async () => {
+        let response = await service.getAllTransactions();
+        //console.log("LoginResponse: " + JSON.stringify(response));
+        if (response.status === 200) {
+          console.log("GetAllTransactions: Login success!");
+          //console.log(JSON.stringify(response.data));
+    
+          setTransactions(response.data);
+        } else {
+          // otherwise login failed with an error message
+          console.log("Error: GetAllOthercustomers failed.");
+        }
+      };
+
     return (
         <div>
             <div>
                 <h1>Your Account</h1>
                 <br/>
 
-                <h3>Name: {props.name}</h3><br/>
-                <h3>Username: {props.username}</h3><br/>
-                <h3>Address: {props.address}</h3><br/>
-                <h3>Phone: {props.phoneNumber}</h3><br/>
-                <h3>Balance: {props.currentBalance}</h3><br/>
+                <h3>Name: {account.name}</h3><br/>
+                <h3>Username: {account.username}</h3><br/>
+                <h3>Address: {account.address}</h3><br/>
+                <h3>Phone: {account.phoneNumber}</h3><br/>
+                <h3>Balance: {account.currentBalance}</h3><br/>
             </div>
             <div>
                 <NavbarBank/>
@@ -21,34 +61,25 @@ function Customer(props) {
             <div>
                 <h2>Recent Transactions</h2>
 
-        <table>
+        <table className="table table-striped table-bordered">
           <thead>
              <tr>
-               <th scope="col">#</th>
-               <th scope="col">Date</th>
-               <th scope="col">Amount</th>
-               <th scope="col">Balance Before</th>
-               <th scope="col">Balance After</th>
-               <th scope="col">Message</th>
+               <th >Date</th>
+               <th >Amount</th>
+               <th >Balance Before</th>
+               <th >Balance After</th>
+               <th >Message</th>
              </tr>
           </thead>
          <tbody>
-         {Transactions &&
-                Transactions.map((transactions) => (
-                  <tr key={transacton.date}>
-                    <td>{customer.amount}</td>
-                    <td>{customer.balanceBefore}</td>
-                    <td>{customer.balanceAfter}</td>
+         {transactions &&
+                transactions.map((transaction) => (
+                  <tr key={transaction.id}>
+                    <td>{transaction.date.toString()}</td>
+                    <td>{transaction.amount}</td>
+                    <td>{transaction.balanceBefore}</td>
+                    <td>{transaction.balanceAfter}</td>
                     <td>{transaction.statusMessage}</td>
-                      <input
-                        name="recieverId"
-                        // key={item}
-                        // id={item}
-                        value={transaction.id}
-                        type="radio"
-                        // onChange={(e) => updateTransfer(e, "recieverId")}
-                        />
-
                   </tr>
                 ))}
 
--- dollars-bank-react-frontend/src/components/NavbarBank.js
+++ dollars-bank-react-frontend/src/components/NavbarBank.js
@@ -1,48 +1,86 @@
-import '../css/NavbarBank.css'
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import { NavLink} from 'react-router-dom';
+import "../css/NavbarBank.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { NavLink } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import HomeIcon from "@material-ui/icons/Home";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  logout: {
+    marginLeft: "auto",
+  },
+}));
 
 function NavbarBank() {
-  
-  return (
-   <div>
-      <nav className="navbar">
-        <div className="nav-container">
-          <NavLink exact to="/" className="glow">
-            Dollars Bank
-          </NavLink>
+  const classes = useStyles();
 
-          <ul className="nav-menu">
-              <li className="nav-item">
-                <NavLink exact to="/deposit" className="nav-links">
-                   Make a Deposit
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink exact to="/withdrawl" className="nav-links">
-                   Make a Withdrawl
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink exact to="/transfer" className="nav-links">
-                   Transfer Funds
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink exact to="/passchange" className="nav-links">
-                  Change Your Password
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink exact to="/logout" className="nav-links">
-                  Logout
-                </NavLink>
-              </li>
-          </ul>
-        </div>
-      </nav>
-  </div>
-  ) 
-} 
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        {/* //home */}
+        <Typography variant="h4" className={classes.title}>
+          DollarsBank Application
+        </Typography>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <HomeIcon />
+          </IconButton>
+
+          <IconButton color="inherit">
+            {" "}
+            <NavLink exact to="/deposit" className="nav-links">
+              Deposit
+            </NavLink>
+          </IconButton>
+          {/* <NavLink to="/"> Dashboard </NavLink> */}
+
+          <IconButton color="inherit">
+            {" "}
+            <NavLink exact to="/withdrawl" className="nav-links">
+              Withdraw
+            </NavLink>
+          </IconButton>
+
+          <IconButton color="inherit">
+            {" "}
+            <NavLink exact to="/transfer" className="nav-links">
+              Transfer
+            </NavLink>
+          </IconButton>
+
+          <IconButton color="inherit" align="right">
+            {" "}
+            <NavLink exact to="/passchange" className="nav-links">
+              Change Password
+            </NavLink>
+          </IconButton>
+          <IconButton className={classes.logout} color="inherit" align="right">
+            {" "}
+            <NavLink exact to="/logout" className="nav-links">
+              Logout
+            </NavLink>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
 export default NavbarBank;
